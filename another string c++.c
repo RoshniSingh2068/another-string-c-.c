84c5a64bb0f9269bb158ea2b2dddcd5162cd677d
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:11:37 +0530

    Create another string c++.c

---
+++ another string c++.c
@@ -0,0 +1,10 @@
+// the sequence // insert the single backslash in the string
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    string txt = " the character \\ is called backslash";
+    cout << txt;
+    return 0;
+}
